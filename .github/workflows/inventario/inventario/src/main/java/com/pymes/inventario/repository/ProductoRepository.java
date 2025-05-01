1edcd92bb6d68ccb1730b0081e10de551eecf3d5
MauPrograma777 <160794185+MauPrograma777@users.noreply.github.com>
2025-05-01 02:11:07 -0600

    Delete .github/workflows/inventario/inventario directory

--- .github/workflows/inventario/inventario/src/main/java/com/pymes/inventario/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pymes.inventario.repository;
-
-import com.pymes.inventario.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
--- .github/workflows/inventario/inventario/src/main/java/com/pymes/inventario/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pymes.inventario.repository;
-
-import com.pymes.inventario.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
